019f23d04cf077374f69d8b90d0b1937bccf0aaf
zonik33 <thezonikk@gmail.com>
2026-03-18 15:37:33 +0300

    fix: remove unused useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { recipes, categories } from "./data/recipes";
 import FoodIllustration from "./components/FoodIllustration";
 import CookingGuide from "./components/CookingGuide";
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {createRoot} from "react-dom/client";
 
 const root = document.getElementById("root");
 createRoot(root).render(
-    <StrictMode>
+    <React.StrictMode>
         <App />
-    </StrictMode>
+    </React.StrictMode>
 );
